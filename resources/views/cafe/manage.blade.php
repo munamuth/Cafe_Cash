630a00dabc827f0ad3ff6f1f63fb422c6f700ba4
ACER <ACER@MUTH>
2018-04-11 16:13:44 +0700

    3 update

--- resources/views/cafe/manage.blade.php
+++ resources/views/cafe/manage.blade.php
@@ -1,27 +1,23 @@
-@extends('layouts.app')
+@extends('cafe_master')
 
 @section('content')
-    <div class="panel-group" id="manage">
-
-      <div class="panel panel-default">
-        <div class="panel-heading">
-            <a data-toggle="collapse" data-parent="#manage" href="#menu">Menu</a>
+    <div class="manage">
+      <div class="card">
+        <div class="card-header">
+          <button class="btn btn-success btn-sm" data-parent="#menage" data-toggle="collapse" data-taget="menu">Menu</button>
+          <button class="btn btn-success btn-sm" data-parent="#menage" data-toggle="collapse" data-taget="new_menu">New</button>
         </div>
-        <div id="menu" class="panel-collapse collapse in">
-          <div class="panel-body">Panel Body</div>
-          <div class="panel-footer">Panel Footer</div>
+        <div class="collapse in">
+          <div class="card-body" id="menu">
+            moo
+          </div>
         </div>
+        <div class="collapse">
+          <div class="card-body" id="new_menu">
+            moo
+          </div>
+        </div>
+        
       </div>
-
-      <div class="panel panel-default">
-        <div class="panel-heading">
-            <a data-toggle="collapse" data-parent="#manage" href="#price">Price List</a>
-        </div>
-        <div id="price" class="panel-collapse collapse">
-          <div class="panel-body">Panel Body</div>
-          <div class="panel-footer">Panel Footer</div>
-        </div>
-      </div>
-
     </div>
 @endsection
